22d0fa583f17fe56db653fbec5572c6e2dd52e93
Mostak Ahamed Nishat <37589801+Mostak-Ahamed-Nishat@users.noreply.github.com>
2022-07-03 00:35:04 +0600

    user profile card added

--- src/pages/singlepage/SinglePage.js
+++ src/pages/singlepage/SinglePage.js
@@ -15,11 +15,29 @@ function SinglePage() {
             <h1>Information</h1>
             <div className="item">
               <img
-                src="https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1600"
                 alt="Item image"
                 className="itemImg"
               />
-              <div className="details"></div>
+              <div className="details">
+                <h1 className="itemTitle">Nishat Mostakim</h1>
+                <div className="detailItem">
+                  <span className="itemKey">EmaiL:</span>
+                  <span className="itemValue">mostakahamed484@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">01778831339</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Bangladesh</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">Dhanmondi,Dhaka,Bangladesh</span>
+                </div>
+              </div>
             </div>
           </div>
           <div className="right">Right</div>
